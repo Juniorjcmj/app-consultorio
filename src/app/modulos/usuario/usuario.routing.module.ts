e7af82951f71bdf8843b20a7320b18a7b1824c4c
José Carlos <jotacfs2010@hotmail.com>
2022-08-15 20:21:56 -0300

    Configurando rota para usuario

--- src/app/modulos/usuario/usuario.routing.module.ts
+++ src/app/modulos/usuario/usuario.routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 
 const routes: Routes = [
-  {path: '',}
+  {path: '', component: UsuarioFormComponent}
 
 ];
 
