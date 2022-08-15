d6779bd174ff06e99e7e3e4307daa967e4e6a019
José Carlos <jotacfs2010@hotmail.com>
2022-08-15 10:34:38 -0300

    configurando a moeda para real brasileiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,13 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoginService } from './modulos/login/login.service';
 import { AuthGuard } from './guards/auth.guard';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+
+// **************Para formatar moeda para real brasileiro************************************
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
+// ***
 
 @NgModule({
   declarations: [
@@ -29,6 +36,10 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
       useClass: AuthInterceptor,
       multi:true
      },
+      // **********Para formatar moeda para real brasileiro**************************
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+    // *
     LoginService,
     AuthGuard
   ],
