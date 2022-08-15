5269505febc67d94749810314bfd30863043ed20
José Carlos <jotacfs2010@hotmail.com>
2022-08-15 10:04:07 -0300

    criando componente de login, home e o serviço de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './modulos/login/login.component';
+import { HomeComponent } from './modulos/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
