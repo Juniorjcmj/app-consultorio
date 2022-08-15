41b031421055f162355a040e1625105a9df08933
José Carlos <jotacfs2010@hotmail.com>
2022-08-15 20:11:58 -0300

    criando componentes para usuario, grupo e permissao

--- src/app/modulos/usuario/usuario.module.ts
+++ src/app/modulos/usuario/usuario.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioService } from './usuario.service';
+import { UsuarioRoutingModule } from './usuario.routing.module';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ UsuarioFormComponent],
   imports: [
     CommonModule,
+    UsuarioRoutingModule
+
   ],
   providers:[UsuarioService]
 })
