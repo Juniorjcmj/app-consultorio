2be75d1c0eab3dbead739d71d4f83454c7bf53a2
José Carlos <jotacfs2010@hotmail.com>
2022-08-15 11:00:28 -0300

    criando um modulo para concentrar os componentes do angular material

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatRadioModule} from '@angular/material/radio';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+  ],
+   exports:
+   [
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatRadioModule
+   ]
+})
+export class MaterialModule { }
