27cd84eaa0037a0bce6481133db8405e966f615a
José Carlos <jotacfs2010@hotmail.com>
2022-09-17 23:58:17 -0300

    Criando layout para página de pacientes

--- src/app/modulos/pacientes/page-pacientes/page-pacientes.component.ts
+++ src/app/modulos/pacientes/page-pacientes/page-pacientes.component.ts
@@ -1,4 +1,9 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { map } from 'rxjs';
+import { UsuarioFormComponent } from '../../usuario/usuario-form/usuario-form.component';
 
 @Component({
   selector: 'app-page-pacientes',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagePacientesComponent implements OnInit {
 
-  constructor() { }
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+
+          { title: 'list', cols: 1, rows: 1 },
+
+        ];
+      }
+
+      return [
+
+        { title: 'list', cols: 2, rows: 5 },
+
+
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver,public dialog: MatDialog,private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  openDialogNovo() {
+    const dialogRef = this.dialog.open(UsuarioFormComponent, {
+      data: {
+
+      },
+    });
   }
 
 }
