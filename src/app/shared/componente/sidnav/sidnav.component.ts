921fa312643ffb38f033a446b4b4f46c47709d6f
José Carlos <jotacfs2010@hotmail.com>
2022-09-19 22:13:28 -0300

    adicionando permissao de administrador para ver a pagina de usuarios

--- src/app/shared/componente/sidnav/sidnav.component.ts
+++ src/app/shared/componente/sidnav/sidnav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/modulos/login/login.service';
 
 @Component({
   selector: 'app-sidnav',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidnavComponent implements OnInit {
 
-  constructor() { }
+  isAdmin: boolean = false;
+
+  constructor(private service: LoginService) {
+
+    let user: any = this.service.decodeJwt();
+    let authorities: string[]  = user['authorities']
+    if(user['authorities'] != undefined){
+      var userAdministrador = authorities.find(x => x ===  "ADMINISTRADOR")
+
+      if(userAdministrador !== undefined){
+        if(userAdministrador){
+          this.isAdmin = true;
+        }
+
+      }
+    }
+
+   }
 
   ngOnInit(): void {
   }
