b7e692d5a03a079ba38f9b8290763f1e67d5f532
José Carlos <jotacfs2010@hotmail.com>
2023-01-11 22:07:52 -0300

    messagens customizadas

--- src/app/services/mensagens.service.ts
+++ src/app/services/mensagens.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class MensagensService {
+export class CustomMensagensService {
 
   constructor() { }
+
+
+  onMessage(mensagem:any, icon: any){
+    Swal.fire({
+      icon: icon,
+      title: mensagem,
+      text: 'Click no botão para sair'
+    })
+  }
+
+  onSuccessSmall(){
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Operação realizada com sucesso!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
 
 
 }
