fe458a2977f794820ce2a6930ba488e9f47804fc
José Carlos <jotacfs2010@hotmail.com>
2023-01-13 23:37:46 -0300

    ajuste nas urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrlAuthenticationServer: 'http://localhost:99/',
-  apiUrlResourceServer: 'http://localhost:99/'
+  apiUrlAuthenticationServer: 'http://casanobre.masterdaweb.net:98/',
+  apiUrlResourceServer: 'http://casanobre.masterdaweb.net:98/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
- apiUrlAuthenticationServer: 'http://localhost:99/',
- apiUrlResourceServer: 'http://localhost:99/'
+  apiUrlAuthenticationServer: 'http://casanobre.masterdaweb.net:98/',
+  apiUrlResourceServer: 'http://casanobre.masterdaweb.net:98/'
 
  //apiUrlAuthenticationServer: 'http://casanobre.masterdaweb.net:99/',
   //apiUrlResourceServer: 'http://casanobre.masterdaweb.net:99/'
