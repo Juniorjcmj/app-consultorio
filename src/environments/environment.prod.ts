daf1881e6d6016f3b4d2bd801ed8d8566b3ed1d3
José Carlos <jotacfs2010@hotmail.com>
2023-01-16 01:14:26 -0300

    publicacao gitPages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net:8443/',
-  // apiUrlResourceServer: 'https://casanobre.masterdaweb.net:8443/'
+  apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net:8443/',
+  apiUrlResourceServer: 'https://casanobre.masterdaweb.net:8443/'
 
-  apiUrlAuthenticationServer: 'https://localhost:8443/',
-  apiUrlResourceServer: 'https://localhost:8443/',
+  // apiUrlAuthenticationServer: 'https://localhost:8443/',
+  // apiUrlResourceServer: 'https://localhost:8443/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: true,
-  apiUrlAuthenticationServer: 'https://localhost:8443/',
-  apiUrlResourceServer: 'https://localhost:8443/',
+  // apiUrlAuthenticationServer: 'https://localhost:8443/',
+  // apiUrlResourceServer: 'https://localhost:8443/',
 
-//  apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net:8443/',
-//   apiUrlResourceServer: 'https://casanobre.masterdaweb.net:8443/'
+ apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net:8443/',
+  apiUrlResourceServer: 'https://casanobre.masterdaweb.net:8443/'
 };
