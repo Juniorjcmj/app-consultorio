b4fbc3e9b86c60d7513c8add9bbbbf6b881c3cda
José Carlos <jotacfs2010@hotmail.com>
2023-01-16 22:37:33 -0300

    antes de remover pwa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: true,
-  // apiUrlAuthenticationServer: 'https://localhost:8443/',
-  // apiUrlResourceServer: 'https://localhost:8443/',
+  apiUrlAuthenticationServer: 'http://localhost:98/',
+  apiUrlResourceServer: 'http://localhost:98/',
 
- apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net:8443/',
-  apiUrlResourceServer: 'https://casanobre.masterdaweb.net:8443/'
+//  apiUrlAuthenticationServer: 'https://casanobre.masterdaweb.net/',
+//   apiUrlResourceServer: 'https://casanobre.masterdaweb.net/'
 };
