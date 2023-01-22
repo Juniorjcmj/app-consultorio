95fd7821b2e165699677fa77c70190c2d77c3c0d
José Carlos <jotacfs2010@hotmail.com>
2023-01-22 13:28:53 -0300

    implementando retorno de filtro atual em manter conciliacao

---
+++ src/app/modulos/conciliacao-cartao/page/list.component.ts
@@ -0,0 +1,345 @@
+import { FiltroConciliacao } from '../model/filtroConciliacao';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ConciliacaoCartaoService } from '../service/conciliacao-service';
+import {
+  ConciliacaoCartao,
+  PageConciliacao,
+  Empresa,
+  Operadora,
+  ConciliacaoCartaoInput,
+} from '../model/conciliacaoCartao';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import * as FileSaver from 'file-saver';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { EmpresaService } from '../../empresa/service/empresa-service';
+
+
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { KeycloakService } from 'keycloak-angular';
+
+import { OperadoraCartaoService } from '../../operadora-cartao/operadora-cartao.service';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
+import { CustomMensagensService } from '../../../services/mensagens.service';
+import { UpdateGenericoConciliacao } from '../model/updateGenericoConciliacao';
+import { FiltroAvancado } from '../../contas-pagar/model/filtro';
+
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  providers: [MessageService, ConfirmationService],
+})
+export class ListComponent implements OnInit {
+
+  @ViewChild('htmlData') htmlData!: ElementRef;
+  conciliacaoDialog!: boolean;
+
+  conciliacao!: ConciliacaoCartao;
+
+  selectedConciliacao!: ConciliacaoCartao[];
+
+  submitted!: boolean;
+
+  statuses!: any[];
+
+  exportColumns: any[] = [];
+
+  conciliacaoXLS!: ConciliacaoCartao[];
+
+  empresas!: Empresa[];
+  operadoras!: Operadora[];
+
+  stateOptions!: any[];
+  statusRecebimento!: any[];
+
+  form!: FormGroup;
+  formDataRecebimento!: FormGroup;
+
+  display: boolean = false;
+
+  pageConciliacao: PageConciliacao = new PageConciliacao();
+
+  // conciliacaoCartoes$: Observable<ConciliacaoCartao[]>;
+  pagina!: ConciliacaoCartao[];
+
+  @ViewChild('myDiv') myDiv!: ElementRef;
+
+  //TRABALHANDO COM FILTROS
+  queryFields = new FormControl();
+  queryFieldsAute = new FormControl();
+  queryFieldsData = new FormControl();
+  queryFieldsIdEmpresa = new FormControl();
+
+  formFilter!: FormGroup;
+
+ updateUtil: UpdateGenericoConciliacao = new UpdateGenericoConciliacao();
+
+
+  constructor(
+    private conciliacaoCartaoService: ConciliacaoCartaoService,
+    private formBuilder: FormBuilder,
+    private confirmationService: ConfirmationService,
+    private spinner: NgxSpinnerService,
+    private empresaService: EmpresaService,
+    private authService: AuthService,
+    private serviceOperadora: OperadoraCartaoService,
+    private customMessage: CustomMensagensService
+  ) {
+
+    this.formFilter = this.formBuilder.group({
+      dtInicio:[''],
+      dtFim:[''],
+      numeroPedido:[''],
+      aute:[''],
+      idEmpresa: [''],
+      idOperadora:[''],
+      dataRecebimento:[''],
+      previsaoRecebimento: [''],
+      tipoOperacao:[''],
+      isRecebido:[''],
+
+    })
+    this.stateOptions = [
+      { label: 'CRÉDITO', value: 'CRÉDITO' },
+      { label: 'DÉBITO', value: 'DÉBITO' },
+    ];
+    this.statusRecebimento = [
+      { label: 'RECEBIDO', value: 'true' },
+      { label: 'NÃO RECEBIDO', value: 'false' },
+    ];
+
+    this.onReload()
+
+    this.serviceOperadora.getAllOperadoraPage(250,0).subscribe(
+      (data) => {
+        this.spinner.hide();
+        this.operadoras = data.content;
+      },
+      (error) => {
+        this.authService.getRedirect401(error.status);
+      }
+    );
+
+    this.empresaService.getAll().subscribe(
+      (data) => {
+        this.empresas = data;
+      },
+      (error) => {
+        this.authService.getRedirect401(error.status);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.spinner.show();
+  }
+
+  filtroAvancado(){
+    this.spinner.show();
+    this.conciliacaoCartaoService.filtroConciliacao(this.formFilter.value).subscribe(
+      (data:any)  =>{
+      this.pageConciliacao = data;
+      this.pagina = this.pageConciliacao.content
+      this.spinner.hide();
+    }
+       );
+  }
+  resetarFiltro(){
+    this.formFilter.reset()
+  }
+
+  openNew() {
+
+    this.form = this.formBuilder.group({
+      id: [null],
+      data: [null, Validators.required],
+      idEmpresa: [null, Validators.required],
+      idOperadora: [null, Validators.required],
+      valorPedido: [null, Validators.required],
+      numeroPedido: [null, Validators.required],
+      aute: [null, Validators.required],
+      tipoOperacao: [null, Validators.required],
+      quemCadastrou:[this.authService.getUser()]
+    });
+
+    this.submitted = false;
+    this.conciliacaoDialog = true;
+  }
+  fabricarExel(event: any) {
+    console.log('emitindo exel');
+  }
+
+  editConciliacao(conciliacao: ConciliacaoCartao) {
+    this.form = this.formBuilder.group({
+      id: [conciliacao.id],
+      data: [conciliacao.data, Validators.required],
+      idEmpresa: [conciliacao.idEmpresa, Validators.required],
+      idOperadora: [conciliacao.idOperadora, Validators.required],
+      valorPedido: [conciliacao.valorPedido, Validators.required],
+      numeroPedido: [conciliacao.numeroPedido, Validators.required],
+      aute: [conciliacao.aute, Validators.required],
+      tipoOperacao: [conciliacao.tipoOperacao, Validators.required],
+      dataRecebimento: [conciliacao.dataRecebimento],
+      quemConferiu:[this.authService.getUser()]
+    });
+    this.conciliacaoDialog = true;
+  }
+
+  deleteConciliacao(record: ConciliacaoCartao) {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir ' + record.numeroPedido + '?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        //codigo para excluir
+        this.conciliacaoCartaoService.delete(record.id).subscribe(
+          (data) => {
+           this.customMessage.onMessage("Operação realizada com sucesso!","success")
+          },
+          (error) => {
+           this.customMessage.onMessage("Operação não realizada!","error")
+          }
+        );
+      },
+    });
+  }
+
+  hideDialog() {
+    this.conciliacaoDialog = false;
+    this.display = false;
+    this.submitted = false;
+  }
+
+  manterConciliacao() {
+    this.spinner.show();
+    this.conciliacaoDialog = false;
+    this.display = false;
+    this.submitted = true;
+    this.conciliacaoCartaoService.manterConciliacao(this.form.value).subscribe(
+      (success:any) => {
+        this.spinner.hide();
+        this.customMessage.onMessage("Operação realizada com sucesso!","success")
+        this. onReload();
+              },
+      (error) => {
+        this.spinner.hide();
+       this.customMessage.onMessage("Operação não realizada!","error")
+
+        return '';
+      }
+    );
+  }
+
+  onReload() {
+    this.conciliacaoCartaoService.filtroConciliacao(this.formFilter.value).subscribe(
+      (data: any) => {
+        this.spinner.hide();
+        this.pagina = data.content;
+        this.conciliacaoXLS = this.pagina;
+      },
+      (error:any) => {
+       this.authService.getRedirect401(error.status);
+       }
+    );
+  }
+  public exportPdf(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });
+  }
+  exportExcel() {
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(this.conciliacaoXLS!);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, 'products');
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE,
+    });
+    FileSaver.saveAs(
+      data,
+      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+    );
+  }
+
+  //TRABALHANDO A PAGINAÇÃO DINAMICAMENTE
+
+
+
+  isConferido(conciliacaoInput: ConciliacaoCartao) {
+    this.spinner.show();
+    this.updateUtil.foiConferido = conciliacaoInput.foiConferido == 'SIM' ? "false" : "true";
+    this.updateUtil.id = conciliacaoInput.id.toString();
+    this.updateUtil.quemConferiu = this.authService.getUser() as string;
+    this.conciliacaoCartaoService
+      .alterarConferidoOuNao(this.updateUtil)
+      .subscribe(
+        (data: any) => {
+          // this.pagina = data
+          this.spinner.hide();
+          this.customMessage.onMessage("Operação realizada com sucesso!","success")
+          this.pagina = data.content;
+        },
+        (error) => {
+          this.spinner.hide();
+          this.customMessage.onMessage("Operação não realizada!","error")
+        }
+      );
+  }
+
+  openDialogDataRecebimento(conciliacao: ConciliacaoCartao) {
+    this.formDataRecebimento = this.formBuilder.group({
+      id: [conciliacao.id],
+      dataRecebimento: [conciliacao.dataRecebimento, Validators.required],
+      foiConferido: [conciliacao.foiConferido],
+      quemConferiu:[this.authService.getUser()]
+    });
+    this.display = true;
+  }
+
+  manterDataRecebimentoConciliacao() {
+    this.spinner.show();
+    this.display = false;
+    this.submitted = true;
+    this.conciliacaoCartaoService
+      .alterarDataRecebimento(this.formDataRecebimento.value)
+      .subscribe(
+        (data: any) => {
+          this.spinner.hide();
+          this.customMessage.onMessage("Operação realizada com sucesso!","success")
+          this.pagina = data.content;
+        },
+        (error) => {
+        this.spinner.hide();
+        this.customMessage.onMessage("Operação não realizada!","error")
+
+          return '';
+        }
+      );
+  }
+
+
+}
