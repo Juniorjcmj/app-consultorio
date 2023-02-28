b26a9f116f2e9332a25e8275786be22eb5e16c05
José Carlos <jotacfs2010@hotmail.com>
2023-02-27 21:18:02 -0300

    ajuste no modelo de Colaborador para cadastros

--- src/app/modulos/colaborador/model/colaborador.ts
+++ src/app/modulos/colaborador/model/colaborador.ts
@@ -1,12 +1,24 @@
-export class Colaborador{
+import { Funcao } from './funcao';
+export class ColaboradorInput{
   id!:number
   nome!: 	string
   identidade!:	string
-  funcao!:	string
+  idFuncao!:	number
   cnh!:	string
   tipoCnh!: 	string
   cpf!:	string
   dataNascimento!:	Date
   ativo!:	boolean
-
   }
+  export class Colaborador{
+    id!:number
+    nome!: 	string
+    identidade!:	string
+    funcao!:	Funcao
+    cnh!:	string
+    tipoCnh!: 	string
+    cpf!:	string
+    dataNascimento!:	Date
+    ativo!:	boolean
+
+    }
