50748ab32c4d4694806db49828051792311d7dec
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2023-11-09 21:35:14 -0300

    limpando projeto

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrlAuthenticationServer: 'https://api-casanobre-production.up.railway.app/',
-  apiUrlResourceServer: 'https://api-casanobre-production.up.railway.app/'
-
-};
